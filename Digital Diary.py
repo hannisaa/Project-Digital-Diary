545f73872e2ee239d1d93a4e1b114e2c092e9531
hannisaa <hilwannisaa@gmail.com>
2025-09-27 20:30:08 +0700

    Digital Diary.py

--- Digital Diary.py
+++ Digital Diary.py
@@ -1,3 +1,4 @@
+from enum import nonmember
 from operator import truediv
 
 import os
@@ -13,15 +14,9 @@ def connect_db():
         passwd="",
         database="db_digital_diary",
     )
-# CLEAR PAGE
-import os
 
-def clear_screen():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
+current_user_id = None
+current_username = None
 
 # 1. user management
 def register_user(username, fullname, password, date_of_birth, gender):
@@ -36,25 +31,25 @@ def register_user(username, fullname, password, date_of_birth, gender):
     print(f"Your User ID is: {user_id}")
     db.close()
 
-current_user_id = None
-current_username = None
 def login_user(username, password):
     global current_user_id, current_username
     db = connect_db()
-    cursor = db.cursor()
-    sql= "SELECT * FROM users WHERE username = %s AND password = %s"
-    cursor.execute(sql, (username, password))
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
     user = cursor.fetchone()
-    db.close()
-    if user:
-        current_user_id = user[0]
-        current_username = user[1]
-        print(f"Welcome back, {current_username}!")
+    if not user:
+        print("User not found")
+        return None
+    if user["password"] == password:
+        current_user_id = user["id_user"]
+        current_username = user["username"]
+        print(f"Welcome back, {current_username}")
         print(f"Your User ID is: {current_user_id}")
         return current_user_id
     else:
         print("Invalid username or password")
         return None
+
 
 def forgot_password():
     db = connect_db()
@@ -62,14 +57,27 @@ def forgot_password():
     print("\n=== Forgot Password ===")
     username = input("Enter username: ")
     date_of_birth = input("Enter date of birth (YYYY-MM-DD): ")
-    sql = "SELECT password FROM users WHERE username = %s AND date_of_birth = %s"
-    cursor.execute(sql, (username,date_of_birth))
+    sql = "SELECT id_user, password FROM users WHERE username = %s AND date_of_birth = %s"
+    cursor.execute(sql, (username, date_of_birth))
     result = cursor.fetchone()
     if result:
-        print(f"Your password: {result[0]}")
+        user_id = result[0]
+        old_password = result[1]
+        print(f"Your current password: {old_password}")
+        choice = input("Do you want to update your password? (y/n): ").lower()
+        if choice == 'y':
+            new_password = input("Enter new password: ")
+            sql_update = "UPDATE users SET password = %s WHERE id_user = %Zs"
+            cursor.execute(sql_update, (new_password, user_id))
+            db.commit()
+            print("Password updated successfully!")
+        else:
+            print("Password not changed.")
     else:
-        print("No matching user found. Please check your user and date of birth")
+        print("No matching user found. Please check your username and date of birth.")
+
     db.close()
+
 
 def after_login_menu(user_id):
     while True:
@@ -136,12 +144,12 @@ def mood_menu(user_id):
         elif choice == "2":
             view_all_mood(user_id)
         elif choice == "3":
-            entry_id = input("Mood ID to update: ")
+            mood_id = input("Mood ID to update: ")
             new_mood = input("New mood: ")
-            update_mood(user_id, entry_id, new_mood)
+            update_mood(user_id, mood_id, new_mood)
         elif choice == "4":
-            entry_id = input("Mood ID to delete: ")
-            delete_diary_entry(user_id, entry_id)
+            mood_id = input("Mood ID to delete: ")
+            delete_diary_entry(user_id, mood_id)
         elif choice == "5":
             break
         else:
@@ -161,14 +169,21 @@ def achievement_menu(user_id):
             description = input("Description: ")
             add_achievement(user_id, title, description)
         elif choice == "2":
-            view_achievement(user_id, title, description)
+            view_achievement(user_id)
         elif choice == "3":
-            entry_id = input("Achievement ID to update: ")
-            new_achievement = input("New achievement: ")
-            update_achievement(user_id, entry_id, new_achievement)
+            try:
+                achievement_id = input("Achievement ID to update: ")
+                new_title = input("New title: ")
+                new_description = input("New desription: ")
+                update_achievement(user_id, achievement_id, new_title, new_description)
+            except ValueError:
+                print("Invalid input. Achievement ID must be a number.")
         elif choice == "4":
-            entry_id = input("Achievement ID to delete: ")
-            delete_achievement(user_id, entry_id)
+            try:
+                achievement_id = int(input("Achievement ID to delete: "))
+                delete_achievement(user_id, achievement_id)
+            except ValueError:
+                print("Invalid input. Achievement ID must be a number.")
         elif choice == "5":
             break
         else:
@@ -237,13 +252,13 @@ def delete_diary_entry(user_id, entry_id):
 
     db.close()
 
-
+# 3. Mood
 def add_mood(id_user, mood):
     db = connect_db()
     cursor = db.cursor()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     sql = "INSERT INTO mood (id_user, date_time, mood) VALUES (%s, %s, %s)"
-    val = (id_user, now, mood)
+    val = (id_user, mood, now)
     cursor.execute(sql, val)
     db.commit()
     print("Mood added successfully!")
@@ -257,12 +272,39 @@ def view_all_mood(id_user):
     moods = cursor.fetchall()
     if moods:
         for mood in moods:
-            print(f" No: {mood[0]}, MOOD: {mood[1]}, DateTime: {mood[2]}")
-
+            print(f" No: {mood[0]}, Mood: {mood[1]}, DateTime: {mood[2]}")
     else:
         print("No mood entries")
     db.close()
 
+def update_mood(user_id, mood_id, new_mood):
+    db = connect_db()
+    cursor = db.cursor()
+    sql = "UPDATE mood SET mood = %s WHERE id_mood = %s AND id_user = %s"
+    cursor.execute(sql, (new_mood, mood_id, user_id))
+    db.commit()
+    print("Mood updated successfully!")
+    db.close()
+
+def delete_mood(user_id, mood_id):
+    db = connect_db()
+    cursor = db.cursor()
+    sql = "DELETE FROM mood WHERE id_mood = %s AND id_user = %s"
+    cursor.execute(sql, (mood_id, user_id))
+    db.commit()
+    print("Mood entry deleted successfully!")
+    sql = ("SELECT id_mood, mood, date_time FROM mood WHERE id_user = %s")
+    cursor.execute(sql, (user_id,))
+    moods = cursor.fetchall()
+    if moods:
+        print("\n=== Remaining Mood Entries ===")
+        for mood in moods:
+            print(f"NO: {mood[0]}, Activities: {mood[1]}, DateTime: {mood[2]}")
+    else:
+        print("No mood entries left.")
+    db.close()
+
+#3. achievement
 def add_achievement(id_user, title, description):
     db = connect_db()
     cursor = db.cursor()
@@ -282,10 +324,44 @@ def view_achievement(id_user):
     cursor.execute(sql, (id_user,))
     results = cursor.fetchall()
     if not results:
-        print("No achievement")
+        print("No achievement found")
     else:
         for achieve in results:
             print(f"No: {achieve[0]}, Title: {achieve[2]}, Description: {achieve[3]}, Date: {achieve[4]}")
+    db.close()
+
+def update_achievement(user_id, achievement_id, new_title, new_description):
+    db = connect_db()
+    cursor = db.cursor()
+    sql = "UPDATE achievement SET title = %s, description = %s WHERE id_achievement = %s AND id_user = %s"
+    cursor.execute(sql, (new_title, new_description, achievement_id, user_id))
+    db.commit()
+    if cursor.rowcount > 0:
+        print("Achievement updated successfully!")
+    else:
+        print("No achievement found")
+    db.close()
+
+def delete_achievement(user_id, achievement_id):
+    db = connect_db()
+    cursor = db.cursor()
+    sql = "DELETE FROM achievement WHERE id_achievement = %s AND id_user = %s"
+    cursor.execute(sql, (achievement_id, user_id))
+    db.commit()
+    if cursor.rowcount > 0:
+        print("Diary entry deleted successfully!")
+    else:
+        print("No achievement found")
+
+    sql = "SELECT id_entry, todays_activities, date_time FROM diary_entry WHERE id_user = %s"
+    cursor.execute(sql, (user_id,))
+    achievement = cursor.fetchall()
+    if achievement:
+        print("\n=== Remaining achievement ===")
+        for achieve in achievement:
+            print(f"NO: {achieve[0]}, Activities: {achieve[1]}, DateTime: {achieve[2]}")
+    else:
+        print("No diary entries left.")
     db.close()
 
 def main():
